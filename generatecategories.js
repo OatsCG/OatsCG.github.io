51870d323f2c580974292c03f37e80739e607d3e
OatsCG <charlesg2003@hotmail.com>
2022-07-27 22:17:55 -0400

    asda

--- generatecategories.js
+++ generatecategories.js
@@ -1,61 +1,102 @@
-function newalbumcategory() {
-    
-    var albumcontainer = `
-    <div class="home-container">
-        <div class="home-category">
-            <h2 class="category-title">
-                New music
-            </h2>
-            <div class="category-music-list" id="music-list">
-                <div class="category-song">
-                    <img class="category-album-art-blur" src="https://images.complex.com/complex/images/c_fill,f_auto,g_center,w_1200/fl_lossy,pg_1/hcjrqlvc6dfhpjxob9nt/cudi">
-                    <a href="album.html">
-                        <img class="category-album-art" src="https://images.complex.com/complex/images/c_fill,f_auto,g_center,w_1200/fl_lossy,pg_1/hcjrqlvc6dfhpjxob9nt/cudi">
-                    </a>
-                    <span class="category-song-title-text">
-                        Clouds (Audio) ft. Langston Bristol, Curren$y
-                    </span>
-                    <span class="category-song-artist-text">
-                        Logic
-                    </span>
-                    <button class="category-play-button">
-                        play
-                    </button>
-                </div>
-                <div class="category-song">
-                    <img class="category-album-art-blur" src="https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w">
-                    <a href="album.html">
-                        <img class="category-album-art" src="https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w">
-                    </a>
-                    <span class="category-song-title-text">
-                        Clouds (Audio) ft. Langston Bristol, Curren$y
-                    </span>
-                    <span class="category-song-artist-text">
-                        Logic
-                    </span>
-                    <button class="category-play-button">
-                        play
-                    </button>
-                </div>
-                <div class="category-song">
-                    <img class="category-album-art-blur" src="https://i.pinimg.com/736x/ea/1f/64/ea1f64668a0af149a3277db9e9e54824.jpg">
-                    <a href="album.html">
-                        <img class="category-album-art" src="https://i.pinimg.com/736x/ea/1f/64/ea1f64668a0af149a3277db9e9e54824.jpg">
-                    </a>
-                    <span class="category-song-title-text">
-                        Clouds (Audio) ft. Langston Bristol, Curren$y
-                    </span>
-                    <span class="category-song-artist-text">
-                        Logic
-                    </span>
-                    <button class="category-play-button">
-                        play
-                    </button>
-                </div>
-            </div>
-            <div class="category-side-graient">
-            </div>
+function newalbumcategory(title, albumsinfo) {
+
+    var albums = [];
+    for (var a = 0; a < albumsinfo.length; a++) {
+        //albumsinfo [id, albumname, artistname, albumartlink]
+        var albumcontainer = `
+        <div class="category-song">
+            <img class="category-album-art-blur" src="${albumsinfo[a][3]}">
+            <a href="album.html">
+                <img class="category-album-art" src="${albumsinfo[a][3]}">
+            </a>
+            <span class="category-song-title-text">
+                ${albumsinfo[a][1]}
+            </span>
+            <span class="category-song-artist-text">
+                ${albumsinfo[a][2]}
+            </span>
+            <button class="category-play-button">
+                play
+            </button>
+        </div>`;
+        albums.push(albumcontainer);
+        
+    }
+    albums = albums.join("");
+
+    var categorycontainer = `
+    <div class="home-category">
+        <h2 class="category-title">
+            ${title}
+        </h2>
+        <div class="category-music-list">
+            ${albums}
+        </div>
+        <div class="category-side-graient">
         </div>
     </div>
-    `
+    <br>`;
+    document.getElementById("home-container").innerHTML += categorycontainer;
 }
+
+
+newalbumcategory("title title", [
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+
+]);
+newalbumcategory("title title", [
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+
+]);
+newalbumcategory("title title", [
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+
+]);
+newalbumcategory("title title", [
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+
+]);
+newalbumcategory("title title", [
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+    [123, "albumtitle", "logic", "https://images.squarespace-cdn.com/content/v1/53b6eb62e4b06e0feb2d8e86/1557342400705-0MBVEXD53MNZZCPDW68F/SamSpratt_Logic_ConfessionsOfADangerousMind_album_artwork.jpg?format=1500w"],
+
+]);
